36ae3a060d52dfadfb8eed6634f6a61205701958
Robert Espinoza <robertdev08@gmail.com>
2021-07-11 21:03:20 -0500

    Create middleware to print logs for all services

--- udagram-user-api/src/server.ts
+++ udagram-user-api/src/server.ts
@@ -6,8 +6,10 @@ import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
+import logger from './logger'
 import {V0_USER_MODELS} from './controllers/v0/model.index';
 
+import * as jwt from 'jsonwebtoken';
 
 (async () => {
   await sequelize.addModels(V0_USER_MODELS);
@@ -27,6 +29,38 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
     origin: config.url,
   }));
+
+  app.use((req, res, next) => {
+
+    let user;
+
+    if (!req.headers || !req.headers.authorization) {
+      const tokenBearer = req.headers.authorization.split(' ');
+        if (tokenBearer.length == 2) {
+          const token = tokenBearer[1];
+          jwt.verify(token, config.jwt.secret, (err, decoded) => {
+            user = decoded
+          });
+        }
+    }
+
+    logger.info({
+      Title: `User ${user} requested for resource`,
+      URL: req.url,
+      Method: req.method,
+      Body: req.body
+    });
+
+    res.on("finish", () => {
+      logger.info({
+        Title: 'Resource response',
+        StatusCode: res.statusCode,
+        StatusMessage: res.statusMessage
+      })
+    });
+
+    next();
+  });
 
   app.use('/api/v0/', IndexRouter);
 
